92050ac156693a1dc3c6415bf3f636f1ae594e62
JayeshScripter <Jayeshs3052@gmail.com>
2025-04-04 17:27:53 +0530

    First commit - added Filesharing app

---
+++ src/main/java/com/example/Filesharingapp/FilesharingApplication.java
@@ -0,0 +1,16 @@
+package com.example.Filesharingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesharingApplication.class, args);
+	}
+
+}
+
+
+//http://localhost:9090/files/
---
+++ src/test/java/com/example/Filesharingapp/FilesharingappApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.Filesharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class FilesharingappApplicationTests {
+
+@SpringBootTest(classes = FilesharingApplication.class)
+class FilesharingApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
